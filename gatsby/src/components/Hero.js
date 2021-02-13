1a358ea9930bc57f121e733b7aa7c65c9fce60b4
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-02-13 07:44:23 -0600

    working version of the typed out Hero, I need to do styling still and figure width

--- gatsby/src/components/Hero.js
+++ gatsby/src/components/Hero.js
@@ -17,24 +17,26 @@ const HeroStyles = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: var(--blue);
+    word-wrap: break-word;
+    white-space: pre;
 
-color: white;
+    color: white;
     cursor: default;
-.var-highlight{
-  color: #C0AD60;
-}
-.tag-highlight {
-color: var(--tag-highlight);
-}
-.string {
-    color: var(--string);
-}
-.misc {
-    color: var(--misc);
-}
-.attribute-name {
-    color: var(--attribute-name);
-}
+    .var-highlight{
+    color: #C0AD60;
+    }
+    .tag-highlight {
+    color: var(--tag-highlight);
+    }
+    .string {
+        color: var(--string);
+    }
+    .misc {
+        color: var(--misc);
+    }
+    .attribute-name {
+        color: var(--attribute-name);
+    }
 
    
 
@@ -62,55 +64,25 @@ export default function Hero() {
     <HeroStyles>
         <Typewriter onInit = {(typewriter) => {
             typewriter.typeString(`
-            <span id="pre" className="code"> &lt;<span className="tag-highlight">ul</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"menu-container"</span>&gt;
-
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"flex-container"</span>&gt;
+            <span id="pre" className="code">
+                &lt;<span className="tag-highlight">ul </span><span className="attribute-name">id</span>=<span className="string">"menu"</span><span className="attribute-name">className</span>=<span className="string">"dropdown-menu"</span>&gt;
+                
+                    &lt;<span className="tag-highlight">li </span><span className="attribute-name">className</span>=<span className="string">"dropdown-content"</span>&lt;<span className="tag-highlight">a </span><span className="attribute-name">className</span>=<span className="string">"learn-more-link"</span><span className="attribute-name">href</span>=<span className="string">"#learn-more"</span>&gt;
+            </span>
             
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-container"</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-1"</span>/<span className="tag-highlight">div</span>&gt;
-
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-2"</span>/<span className="tag-highlight">div</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-3"</span>/<span className="tag-highlight">div</span>&gt;</span>
         `).start();
         }} />
         
-        <div id="app"></div>
-{/*         
-        <pre> 
-            &lt;<span className="tag-highlight">ul</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"menu-container"</span>&gt;
-
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"flex-container"</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-container"</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-1"</span>/<span className="tag-highlight">div</span>&gt;
-
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-2"</span>/<span className="tag-highlight">div</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-3"</span>/<span className="tag-highlight">div</span>&gt;
-        </pre>< */}
-
-        {/* <pre>
-            
-            <br /><br /><br /><br /><br />
-            &lt;<span className="tag-highlight">ul</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"menu-container"</span>&gt;
-
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"flex-container"</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-container"</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-1"</span>/<span className="tag-highlight">div</span>&gt;
-
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-2"</span>/<span className="tag-highlight">div</span>&gt;
-            
-            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-3"</span>/<span className="tag-highlight">div</span>&gt;
-            
-            <br />
-            {/* &lt;div className="bar-container"&gt; &lt;div className="bar-1"&gt;&lt;/div&gt; &lt;div className="bar-2"&gt;&lt;/div&gt; &lt;div className="bar-3"&gt;&lt;/div&gt; &lt;/div&gt; &lt;div className="logo-container"&gt; &lt;a className="logo-link" href="https://www.joefink.dev/"&gt; &lt;Logo&gt;&lt;/Logo&gt; &lt;/a&gt; &lt;/div&gt; &lt;div id="menu" className="dropdown-menu"&gt; &lt;li className="dropdown-content"&gt;&lt;a className="learn-more-link" href="#learn-more"&gt;<span className="navigation">About</span>&lt;/a&gt;&lt;/li&gt; &lt;li className="dropdown-content"&gt;&lt;a className="education-link" href="#education"&gt;<span className="navigation">Education</span>&lt;/a&gt;&lt;/li&gt; &lt;li className="dropdown-content"&gt;&lt;a className="technology-link" href="#technology"&gt;<span className="navigation">Technologies</span>&lt;/a&gt;&lt;/li&gt; &lt;/div&gt; &lt;/div&gt; &lt;/ul&gt; &lt;div&gt; &lt;/div&gt; */}
-        {/* </pre> */} 
+        <div>
+            <span id="pre" className="code">
+                &lt;<span className="tag-highlight">ul </span><span className="attribute-name">id</span>=<span className="string">"menu"</span><span className="attribute-name">className</span>=<span className="string">"dropdown-menu"</span>&gt;
+                
+                &lt;<span className="tag-highlight">li </span><span className="attribute-name">className</span>=<span className="string">"dropdown-content"</span>&lt;<span className="tag-highlight">a </span><span className="attribute-name">className</span>=<span className="string">"learn-more-link"</span><span className="attribute-name">href</span>=<span className="string">"#learn-more"</span>&gt;
+            </span>
+        </div>
     </HeroStyles> 
+
+    
     )
     
 }
