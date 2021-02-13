cba7075a5645b567622cb106b676dfb8f9261549
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-02-13 06:55:55 -0600

    first working version of Hero, add typewriter-effect from npm. Text still needs to be styled

--- gatsby/src/components/Hero.js
+++ gatsby/src/components/Hero.js
@@ -1,70 +1,116 @@
 import React from 'react';
 // import { graphql, useStaticQuery } from 'gatsby';
 import styled from 'styled-components';
+import Typewriter from 'typewriter-effect';
+
 // import BackgroundImage from 'gatsby-background-image';
 
 const HeroStyles = styled.div`
     --blue: #193549;
-    --blueHighlight: #1F4662;
-    --blueHighlight2: #234E6D;
-    --blueDark: #122738;
-    --yellow: #ffc600;
-    --hotPink: #ff0088;
-    --blushPink: #ff628c;
-    --orange: #ff9d00;
-    width: 100%;
-    height: 100%;
+    --tag-highlight: #9EFFFF; //baby blue
+    --attribute-name:#FFC600; //orang-ish
+    --string: #A5FF90; //lime green
+    --source: #FFFFFF; //white
+    --misc: #FF9D00; //dark orange, used for equal sign
+
+
+    width: 100vw;
+    height: 100vh;
     background-color: var(--blue);
-    color: var(--blueHighlight2);
-    /* background-position: bottom center; */
-    /* background-repeat: none; */
-    /* background-size: cover; */
-    pre {
+
+color: white;
+    cursor: default;
+.var-highlight{
+  color: #C0AD60;
+}
+.tag-highlight {
+color: var(--tag-highlight);
+}
+.string {
+    color: var(--string);
+}
+.misc {
+    color: var(--misc);
+}
+.attribute-name {
+    color: var(--attribute-name);
+}
+
+   
+
+ #pre {
+        margin: 0;
         font-weight: bold;
         font-size: 2.1em;
         line-height: 1.25;
         font-family: monospace;
-        white-space: pre-wrap;
+        white-space: pre;
     }
-    code {
-        white-space: pre-wrap;
+    .code {
+        white-space: normal;
+        text-indent: 150px;
     }
+    .navigation:hover {
+        color: white;
+    }
+ 
     `;
 
-export default function Hero({ className }) {
+export default function Hero() {
+        
     return (
     <HeroStyles>
-        <pre>
-            <code>
+        <Typewriter onInit = {(typewriter) => {
+            typewriter.typeString(`
+            <span id="pre" className="code"> &lt;<span className="tag-highlight">ul</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"menu-container"</span>&gt;
+
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"flex-container"</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-container"</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-1"</span>/<span className="tag-highlight">div</span>&gt;
+
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-2"</span>/<span className="tag-highlight">div</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-3"</span>/<span className="tag-highlight">div</span>&gt;</span>
+        `).start();
+        }} />
+        
+        <div id="app"></div>
+{/*         
+        <pre> 
+            &lt;<span className="tag-highlight">ul</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"menu-container"</span>&gt;
+
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"flex-container"</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-container"</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-1"</span>/<span className="tag-highlight">div</span>&gt;
+
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-2"</span>/<span className="tag-highlight">div</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-3"</span>/<span className="tag-highlight">div</span>&gt;
+        </pre>< */}
+
+        {/* <pre>
+            
             <br /><br /><br /><br /><br />
-            &lt;ul className="menu-container"&gt;<br />
-                &lt;div className="flex-container"&gt; 
-            &lt;div className="bar-container"&gt; &lt;div className="bar-1"&gt;&lt;/div&gt; &lt;div className="bar-2"&gt;&lt;/div&gt; &lt;div className="bar-3"&gt;&lt;/div&gt; &lt;/div&gt; &lt;div className="logo-container"&gt; &lt;a className="logo-link" href="https://www.joefink.dev/"&gt; &lt;Logo&gt;&lt;/Logo&gt; &lt;/a&gt; &lt;/div&gt; &lt;div id="menu" className="dropdown-menu"&gt; &lt;li className="dropdown-content"&gt;&lt;a className="learn-more-link" href="#learn-more"&gt;About&lt;/a&gt;&lt;/li&gt; &lt;li className="dropdown-content"&gt;&lt;a className="education-link" href="#education"&gt;Education&lt;/a&gt;&lt;/li&gt; &lt;li className="dropdown-content"&gt;&lt;a className="technology-link" href="#technology"&gt;Technologies&lt;/a&gt;&lt;/li&gt; &lt;/div&gt; &lt;/div&gt; &lt;/ul&gt; &lt;div&gt; &lt;/div&gt;
-            </code>
-        </pre>
+            &lt;<span className="tag-highlight">ul</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"menu-container"</span>&gt;
+
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"flex-container"</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-container"</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-1"</span>/<span className="tag-highlight">div</span>&gt;
+
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-2"</span>/<span className="tag-highlight">div</span>&gt;
+            
+            &lt;<span className="tag-highlight">div</span> <span className="attribute-name">className</span><span className="misc">=</span><span className="string">"bar-3"</span>/<span className="tag-highlight">div</span>&gt;
+            
+            <br />
+            {/* &lt;div className="bar-container"&gt; &lt;div className="bar-1"&gt;&lt;/div&gt; &lt;div className="bar-2"&gt;&lt;/div&gt; &lt;div className="bar-3"&gt;&lt;/div&gt; &lt;/div&gt; &lt;div className="logo-container"&gt; &lt;a className="logo-link" href="https://www.joefink.dev/"&gt; &lt;Logo&gt;&lt;/Logo&gt; &lt;/a&gt; &lt;/div&gt; &lt;div id="menu" className="dropdown-menu"&gt; &lt;li className="dropdown-content"&gt;&lt;a className="learn-more-link" href="#learn-more"&gt;<span className="navigation">About</span>&lt;/a&gt;&lt;/li&gt; &lt;li className="dropdown-content"&gt;&lt;a className="education-link" href="#education"&gt;<span className="navigation">Education</span>&lt;/a&gt;&lt;/li&gt; &lt;li className="dropdown-content"&gt;&lt;a className="technology-link" href="#technology"&gt;<span className="navigation">Technologies</span>&lt;/a&gt;&lt;/li&gt; &lt;/div&gt; &lt;/div&gt; &lt;/ul&gt; &lt;div&gt; &lt;/div&gt; */}
+        {/* </pre> */} 
     </HeroStyles> 
     )
-//     const data = useStaticQuery(
-//         graphql`
-//         query {
-//             desktop: file(relativePath: { eq: "background-working.JPG" }) {
-//                 childImageSharp {
-//                     fluid(quality: 90, maxWidth: 1920) {
-//                         ...GatsbyImageSharpFluid_withWebp
-//                     }
-//                 }
-//             }
-//         }`
-//     )
     
-// const imageData = data.desktop.childImageSharp.fluid;
-// return (
-//     <HeroStyles>
-//         <BackgroundImage
-//         Tag="section"
-//         className={className}
-//         fluid={imageData}
-//         backgroundColor={`#040e18`}>background</BackgroundImage>
-//     </HeroStyles>
-// )
 }
