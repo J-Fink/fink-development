84497b8f7eb9bb2bfd84e943942572b8a8a58cb2
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-02-19 23:58:20 -0600

    add packages

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,9 +1,23 @@
-module.exports = {
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env'})
+export default {
   siteMetadata: {
-    title: "Fink Development Site",
+    title: `Fink Development Site`,
+    siteUrl: `https://www.joefink.dev`,
+    description: `Think Fink for Development`,
   },
   plugins: [
     "gatsby-plugin-styled-components",
+    {
+      //this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'szyp2rot',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
