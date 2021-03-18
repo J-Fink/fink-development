bb058e3d00020bdc10b75b600f981497ab316789
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-03-18 00:04:54 -0500

    update the style of the basic view of each page

--- gatsby/src/styles/BasicPageStyles.js
+++ gatsby/src/styles/BasicPageStyles.js
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 const BasicPageStyles = styled.div`
     margin-top: var(--header-height);
-
+    font-size: var(--nav-font-size);
+    text-align: center;
+    ul {
+        width: 30%;
+        margin: auto;
+    }
 `;
 export default BasicPageStyles;
