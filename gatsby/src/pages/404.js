7b7c2c673234ce1f56b8e26efb56f3a9b10265fe
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-04-10 11:19:04 -0500

    style 404 page

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import BasicPageStyles from '../styles/BasicPageStyles';
+
+//TODO: style this so you can actually see the message
+
 
 export default function FourOhFourPage() {
-  return <p>Howdy, That page doesn't exist... Yet???</p>
+  return (
+      <BasicPageStyles>
+        <p>Howdy, That page doesn't exist... Yet???</p>
+      </BasicPageStyles>
+
+  )
 };
