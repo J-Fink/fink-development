9d4867747b5a72b4666af6e71c6d5558a8da5e32
J-Fink <57537040+J-Fink@users.noreply.github.com>
2021-04-27 22:38:27 -0500

    style about page more, making it more mobile friendly

--- gatsby/src/pages/about.js
+++ gatsby/src/pages/about.js
@@ -16,19 +16,24 @@ const AboutStyles = styled.div`
     width: 100vw;
     --avatarImgHeight: 400px;
     --avatarImgWidth: 400px;
-    --avatarImgLeft: -58vw;
+    /* --avatarImgLeft: -58vw; */
     --introTop: -483px;
     --introLeft: 145px;
     --h1Top: -445px;
-    .test {
+    .flex-container {
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: center;
+    }
+    .mobile-image {
         border-radius:50%;
     }
     .gatsby-image-wrapper {
         height: var(--avatarImgHeight);
         width: var(--avatarImgWidth);
         border-radius: 50%;
-        margin-top: 55px;
-        margin-left: var(--avatarImgLeft);
+        /* margin-top: 95px; */
+        /* margin-left: var(--avatarImgLeft); */
         overflow: hidden;
     }
     .cotton-candy {
@@ -44,16 +49,15 @@ const AboutStyles = styled.div`
         border-radius: 50%;
     } */
     h1 {
-        position: relative;
       top: var(--h1Top);
       font-size: 9rem;
       width: 50vw;
-      margin: auto;
+      margin: 45px auto 0 auto;
       line-height: normal;
     }
     @media (max-width: 1315px) {
-        --avatarImgHeight: 215px;
-        --avatarImgWidth: 215px;
+        /* --avatarImgHeight: 215px;
+        --avatarImgWidth: 215px; */
         /* --introTop: -352px; */
         /* --h1Top: -287px; */
 
@@ -61,12 +65,12 @@ const AboutStyles = styled.div`
 
     }
     @media (max-width: 800px) {
-        --introLeft: 1px;
+        /* --introLeft: 1px;
         --h1Top: -75px;
         --avatarImgLeft: -26vw;
-        --introTop: -154px;
+        --introTop: -154px; */
     }
-    .intro {
+    /* .intro {
         position: relative;
         top: var(--introTop);
         left: var(--introLeft);
@@ -81,14 +85,14 @@ const AboutStyles = styled.div`
         width: 55vw;
         text-align: left;
         margin: 0 auto;
-    }
-    .info p a {
+    } */
+    p a {
         text-decoration: none;
         font-style: italic;
         color: var(--textColor);
     }
-    .info p a:hover {
-        color: var(--textColorHover);
+    p a:hover {
+        color: var(--navTextHoverColor);
     }
   
 `;
@@ -100,51 +104,44 @@ export default function AboutPage() {
             
             <BasicPageStyles>
                 <AboutStyles>
+                        <h1>About</h1>
+                    <span className="flex-container">
+                        <p>
+                            Hi, I'm Joe!<br />
+                            I'm a husband, father, and a self-taught web<br /> developer, from St. Paul, Minnesota<br />
+                        </p>
                     <StaticImage
-                        imgClassName="test" //this allows me to add border radius to the image directly, because when it was on the wrapper it was not showing up on mobile
+                        imgClassName="mobile-image" //this allows me to add border radius to the image directly, because when it was on the wrapper it was not showing up on mobile
                         alt="Joe speaks into a microphone"
                         src="../assets/images/joeFromTheSide.jpg"
                         placeholder="traced SVG"
                         // layout="fixed"
                         width={400}
                         height={400}
-                    />
-                    <span className="container">
-                        {/* <div> */}
-                        <h1>About</h1>
-                        <section className="intro">
-                                <p>
-                                    Hi, I'm Joe!<br />
-                                    I'm a husband, father, and a self-taught web<br /> developer, from St. Paul, Minnesota<br />
-                                </p>
-                        </section>
-                    </span>
-                <section className="info">
-                        <p>
-                            I work for a nonprofit organization where I wear many hats. From maintaining our website(built with Drupal 7), managing livestreaming our services, to all around logistics. I believe having a smooth workflow makes life easier, at the same time I find it helpful to figure out the desired outcome first then find a path to achieve that outcome. In my free time I make websites, and like to use my new found skills to help nonprofit organizations.
-                        </p>
-                </section>
-                <section className="info">
-                        <p>
-                            I started to learn how to program in August of 2019. There has been so much I have learned so far(and yet still so much more!), you can check out my GitHub <a href="https://github.com/J-Fink" rel="noreferrer nofollow">here</a>.
-                        </p>
-                        <p>
-                            I have a lot of interests, when the COVID reality set in I picked up another hobby. My kids love it
-                        </p>
-                        <StaticImage className="cotton-candy"
-                        imgClassName="test"
-                        alt="Joe speaks into a microphone"
-                        src="../assets/images/joeHoldsCottonCandy.jpg"
-                        placeholder="traced SVG"
-                        width={400}
-                        height={400}
-                        // objectFit={cover}
-                        // css={`
-                        /* border: 1px solid green; */
-                        /* border-radius: 100%;  */
-                        // `}
                         />
-                </section>
+                    </span>
+                    
+                    
+                    <p>
+                        I work for a nonprofit organization where I wear many hats. From maintaining our website(built with Drupal 7), managing livestreaming our services, to all around logistics. I believe having a smooth workflow makes life easier, at the same time I find it helpful to figure out the desired outcome first then find a path to achieve that outcome. In my free time I make websites, and like to use my new found skills to help nonprofit organizations.
+                    </p>
+                    
+                    <p>
+                        I started to learn how to program in August of 2019. There has been so much I have learned so far(and yet still so much more!), you can check out my GitHub <a href="https://github.com/J-Fink" rel="noreferrer nofollow">here</a>.
+                    </p>
+                    <p>
+                        I have a lot of interests, when the COVID reality set in I picked up another hobby. My kids love it
+                    </p>
+                    
+                    <StaticImage className="cotton-candy"
+                    imgClassName="mobile-image"
+                    alt="Joe speaks into a microphone"
+                    src="../assets/images/joeHoldsCottonCandy.jpg"
+                    placeholder="traced SVG"
+                    width={400}
+                    height={400}
+                  />
+
                 </AboutStyles>
             </BasicPageStyles>
         </>
