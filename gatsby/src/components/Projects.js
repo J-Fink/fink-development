d9691fb287f2a591ba22c15a0d9472b215a958b6
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-11-07 15:05:54 -0600

    update Projects component, and style for the projects component

--- gatsby/src/components/Projects.js
+++ gatsby/src/components/Projects.js
@@ -5,19 +5,60 @@ import styled from 'styled-components';
 const ProjectsContainer = styled.div`
     display: grid;
     text-align: center;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
     max-width: 800px;
     margin: auto;
 `;
 
 const ProjectItem = styled.div`
-/* display: grid;   */
+display: grid;  
+grid-template-rows: 4% 1fr;
+grid-gap: 0rem;
+justify-content: center;
+.description {
+        font-style: italic;
+        max-width: 250px;
+        margin: 35px auto auto auto;
+    }
+    a {
+        text-decoration: none;
+        color: var(--textColor);
+        height: fit-content;
+        margin: 0 auto;
+        box-shadow: rgb(0 0 0 / 20%) 2px 2px 3px;
+        transform: skew(var(--postTitleSkew));
+        background-color: var(--postTitleColor);
+        :after {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            width: 0%;
+            content: "";
+            color: transparent;
+            background: var(--navTextHoverColor);
+            height: 3px;
+            transition: all 0.25s ease-in-out;
+        }
+        span {
+            display: inline-block; //must add this or the below transform won't work
+            transform: skew(calc(-1 * var(--postTitleSkew)));
+            padding: 5px 10px;
+        }
+    }
+    a:hover, :focus {
+        /* color: var(--textColorHover); */
+        color: white;
+        ::after {
+        width: var(--navUnderlineWidth);
+    }
+    }
 
 `;
 
 export default function Projects() {
     const data = useStaticQuery(graphql`
-    query y{
+    query {
     allMarkdownRemark(limit: 10) {
         edges {
             node {
